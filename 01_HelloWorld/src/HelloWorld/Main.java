317c848ab5e2b137f7903ccf14d763954120b28e
Philipp Schilk <pschilk@stu.iics.k12.tr>
2016-02-29 22:10:26 +0200

    Added examples of comments and showed solution to extention

--- 01_HelloWorld/src/HelloWorld/Main.java
+++ 01_HelloWorld/src/HelloWorld/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		// This is a comment :)
+		// Here is what you would have to write to print Where am I?
+		// System.out.println("Where am I?");
+		
+		/*
+		 * 
+		 * This is also a comment....! The computer ignores me here :)
+		 * 
+		 */
+		
 
 	}
 
